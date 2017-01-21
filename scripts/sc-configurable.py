1570cb1350aae66a12dd145533240aef3a6eb8f8
Mike Cronce <mike@quadra-tec.net>
2017-01-21 12:57:13 -0500

    scripts/sc-configurable.py:  Tabs and whitespace!

--- scripts/sc-configurable.py
+++ scripts/sc-configurable.py
@@ -34,72 +34,74 @@ from steamcontroller.daemon import Daemon
 import gc
 
 def evminit():
-    evm = EventMapper()
-    evm.setPadMouse(Pos.RIGHT)
-    evm.setPadScroll(Pos.LEFT)
-    evm.setStickButtons([Keys.KEY_UP,
-                         Keys.KEY_LEFT,
-                         Keys.KEY_DOWN,
-                         Keys.KEY_RIGHT])
+	evm = EventMapper()
+	evm.setPadMouse(Pos.RIGHT)
+	evm.setPadScroll(Pos.LEFT)
+	evm.setStickButtons([
+		Keys.KEY_UP,
+		Keys.KEY_LEFT,
+		Keys.KEY_DOWN,
+		Keys.KEY_RIGHT
+	])
 
-    evm.setTrigButton(Pos.LEFT, Keys.BTN_RIGHT)
-    evm.setTrigButton(Pos.RIGHT, Keys.BTN_LEFT)
+	evm.setTrigButton(Pos.LEFT, Keys.BTN_RIGHT)
+	evm.setTrigButton(Pos.RIGHT, Keys.BTN_LEFT)
 
-    evm.setButtonAction(SCButtons.LB, Keys.KEY_VOLUMEDOWN)
-    evm.setButtonAction(SCButtons.RB, Keys.KEY_VOLUMEUP)
+	evm.setButtonAction(SCButtons.LB, Keys.KEY_VOLUMEDOWN)
+	evm.setButtonAction(SCButtons.RB, Keys.KEY_VOLUMEUP)
 
-    evm.setButtonAction(SCButtons.STEAM, Keys.KEY_HOMEPAGE)
+	evm.setButtonAction(SCButtons.STEAM, Keys.KEY_HOMEPAGE)
 
-    evm.setButtonAction(SCButtons.A, Keys.KEY_ENTER)
-    evm.setButtonAction(SCButtons.B, Keys.KEY_BACKSPACE)
-    evm.setButtonAction(SCButtons.X, Keys.KEY_ESC)
-    evm.setButtonAction(SCButtons.Y, Keys.KEY_PLAYPAUSE)
+	evm.setButtonAction(SCButtons.A, Keys.KEY_ENTER)
+	evm.setButtonAction(SCButtons.B, Keys.KEY_BACKSPACE)
+	evm.setButtonAction(SCButtons.X, Keys.KEY_ESC)
+	evm.setButtonAction(SCButtons.Y, Keys.KEY_PLAYPAUSE)
 
-    evm.setButtonAction(SCButtons.START, Keys.KEY_NEXTSONG)
-    evm.setButtonAction(SCButtons.BACK, Keys.KEY_PREVIOUSSONG)
+	evm.setButtonAction(SCButtons.START, Keys.KEY_NEXTSONG)
+	evm.setButtonAction(SCButtons.BACK, Keys.KEY_PREVIOUSSONG)
 
-    evm.setButtonAction(SCButtons.LGRIP, Keys.KEY_BACK)
-    evm.setButtonAction(SCButtons.RGRIP, Keys.KEY_FORWARD)
+	evm.setButtonAction(SCButtons.LGRIP, Keys.KEY_BACK)
+	evm.setButtonAction(SCButtons.RGRIP, Keys.KEY_FORWARD)
 
-    evm.setButtonAction(SCButtons.LPAD, Keys.BTN_MIDDLE)
-    evm.setButtonAction(SCButtons.RPAD, Keys.KEY_SPACE)
+	evm.setButtonAction(SCButtons.LPAD, Keys.BTN_MIDDLE)
+	evm.setButtonAction(SCButtons.RPAD, Keys.KEY_SPACE)
 
-    return evm
+	return evm
 
 class SCDaemon(Daemon):
-    def run(self):
-        evm = evminit()
-        sc = SteamController(callback=evm.process)
-        sc.run()
-        del sc
-        del evm
-        gc.collect()
+	def run(self):
+		evm = evminit()
+		sc = SteamController(callback=evm.process)
+		sc.run()
+		del sc
+		del evm
+		gc.collect()
 
 if __name__ == '__main__':
-    import argparse
+	import argparse
 
-    def _main():
-        parser = argparse.ArgumentParser(description=__doc__)
-        parser.add_argument('command', type=str, choices=['start', 'stop', 'restart', 'debug'])
-        parser.add_argument('-i', '--index', type=int, choices=[0,1,2,3], default=None)
-        args = parser.parse_args()
-        if args.index != None:
-            daemon = SCDaemon('/tmp/steamcontroller{:d}.pid'.format(args.index))
-        else:
-            daemon = SCDaemon('/tmp/steamcontroller.pid')
+	def _main():
+		parser = argparse.ArgumentParser(description=__doc__)
+		parser.add_argument('command', type=str, choices=['start', 'stop', 'restart', 'debug'])
+		parser.add_argument('-i', '--index', type=int, choices=[0,1,2,3], default=None)
+		args = parser.parse_args()
+		if args.index != None:
+			daemon = SCDaemon('/tmp/steamcontroller{:d}.pid'.format(args.index))
+		else:
+			daemon = SCDaemon('/tmp/steamcontroller.pid')
 
-        if 'start' == args.command:
-            daemon.start()
-        elif 'stop' == args.command:
-            daemon.stop()
-        elif 'restart' == args.command:
-            daemon.restart()
-        elif 'debug' == args.command:
-            try:
-                evm = evminit()
-                sc = SteamController(callback=evm.process)
-                sc.run()
-            except KeyboardInterrupt:
-                return
+		if 'start' == args.command:
+			daemon.start()
+		elif 'stop' == args.command:
+			daemon.stop()
+		elif 'restart' == args.command:
+			daemon.restart()
+		elif 'debug' == args.command:
+			try:
+				evm = evminit()
+				sc = SteamController(callback=evm.process)
+				sc.run()
+			except KeyboardInterrupt:
+				return
 
-    _main()
+	_main()
