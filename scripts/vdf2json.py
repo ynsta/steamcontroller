fe0b289b096c68d8cb6022d0e7a824e9b0c676ea
Mike Cronce <mike@quadra-tec.net>
2017-01-21 14:19:20 -0500

    Revert "scripts/vdf2json.py:  Added "json" package import, join_duplicate_keys() function, and vdf2sensible_json() function"
    This reverts commit f681c730bc869f2fee2e2e7de64a913adc0a6327.

--- scripts/vdf2json.py
+++ scripts/vdf2json.py
@@ -22,24 +22,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-import json
 from shlex import shlex
-
-def join_duplicate_keys(ordered_pairs): # {{{
-	d = {}
-	for k, v in ordered_pairs:
-		if k in d:
-			if(type(d[k]) == list):
-				d[k].append(v)
-			else:
-				newlist = []
-				newlist.append(d[k])
-				newlist.append(v)
-				d[k] = newlist
-		else:
-			d[k] = v
-	return d
-# }}}
 
 def vdf2json(stream):
 
@@ -78,18 +61,6 @@ def vdf2json(stream):
                 if ntok != '}':
                     jbuf += ','
                 jbuf += '\n'
-
-def vdf2sensible_json(stream):
-	# Since /controller_mappings/group is a key duplicated numerous times, it
-	#    makes it cumbersome to use.  This changes /controller_mappings/group
-	#    to be a single-use key with a dict in it; each object in the dict is a
-	#    one of these separate "group" objects, and the keys to the dict are
-	#    the "id" fields of these objects.
-
-	obj = json.loads(vdf2json(stream), object_pairs_hook = join_duplicate_keys)
-	obj['controller_mappings']['group'] = {group['id'] : group for group in obj['controller_mappings']['group']}
-	return json.dumps(obj)
-
 def main():
     """
     Read Steam vdf and write json compatible conversion
