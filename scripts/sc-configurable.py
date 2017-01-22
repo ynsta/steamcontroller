5b7dee3e0ee9b40e0df4d6e9ec072ec5c2c1d005
Mike Cronce <mike@quadra-tec.net>
2017-01-22 13:02:25 -0500

    scripts/sc-configurable.py:  get_binding():  getattr() -> __getattr__()

--- scripts/sc-configurable.py
+++ scripts/sc-configurable.py
@@ -74,7 +74,7 @@ def get_binding(group_inputs, input_name, activator): # {{{
 
 	# TODO:  mouse_wheel, mouse_button, mode_shift ... more?
 	if(binding[0] == 'key_press'):
-		return Keys.getattr('KEY_' + binding[1])
+		return Keys.__getattr__('KEY_' + binding[1])
 # }}}
 
 def set_trackpad_config(evm, pos, group): # {{{
