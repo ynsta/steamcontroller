debe38cbee48e0adb9183bfade7a618f5d3ee98a
Mike Cronce <mike@quadra-tec.net>
2017-01-22 13:24:22 -0500

    scripts/sc-configurable.py:  get_binding():  Added fallback return None

--- scripts/sc-configurable.py
+++ scripts/sc-configurable.py
@@ -78,6 +78,8 @@ def get_binding(group_inputs, input_name, activator): # {{{
 	# TODO:  mouse_wheel, mouse_button, mode_shift ... more?
 	if(binding[0] == 'key_press'):
 		return Keys.__getattr__('KEY_' + binding[1])
+
+	return None
 # }}}
 
 def set_trackpad_config(evm, pos, group): # {{{
